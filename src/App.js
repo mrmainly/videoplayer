bc7e5867bb6dc247c06c521d27a1af729b331613
ernest <ernest_everstov@mail.ru>
2021-11-19 01:41:18 +0900

    added videoplayer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import ReactPlayer from 'react-player'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: 'white' }}>Video player for you</h1>
+      <div className="videoPlayer" >
+        <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' width="100%" height="800px" />
+      </div>
     </div>
   );
 }
